003cb301fd6b53ae035f4f31e30b5296cfbfaec3
Majid Aziz <azizmajid34@gmail.com>
2022-11-21 10:27:30 +0500

    amazon-clone 1file startup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5aDYf_SR0_07y09PuNVriqX4H5M12q6o",
+    authDomain: "clone-b9d82.firebaseapp.com",
+    projectId: "clone-b9d82",
+    storageBucket: "clone-b9d82.appspot.com",
+    messagingSenderId: "877865151972",
+    appId: "1:877865151972:web:c48a20d4861177a0b4ebf1",
+    measurementId: "G-X67J118MLD"
+  };
